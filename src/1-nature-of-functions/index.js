7ba8c876b15c9530841e0df524c321cf6f90b2ff
Meher Ranjan Howji <hmeherranjan@gmail.com>
2021-10-20 11:14:30 +0530

    added code samples and final section

--- src/1-nature-of-functions/index.js
+++ src/1-nature-of-functions/index.js
@@ -4,10 +4,12 @@
 // But functions are usually more complex in nature. You can store operations in them which can be performed later.
 () => 0		// prints back the same function on the console
 
+
 // 2
 // Based on Value Type and Reference Type as a mechanism to distinguish nature of data.
 // A function is based on Reference Type just like objects and arrays.
 (() => 0) === (() => 0)		// prints false on the console
+
 
 // 3 
 // The way we apply function is by passing 0 or more arguments to it. 
@@ -15,6 +17,7 @@
 fn_expression( args1, args2 )
 // or
 ( () => 0 )()      // prints 0 on the console
+
 
 // 4 
 // We can return a value back from function by putting a value to the right of the arrow
@@ -25,9 +28,11 @@ fn_expression( args1, args2 )
      (() => 0)() 
 )()
 
+
 // 5
 // Similarly, we can even put a block next to the arrow in the function
 ( () => {} )()      // prints undefined on the console
+
 
 // 6
 // undefined is absence of value and its like a value and of value type
@@ -42,4 +47,23 @@ void 0
 // using undefined direclty is risky 
 // hence void seems like a good choice
 
+
 // 7 
+// In simple words, blocks are JS statements separated by semicolon or not(babel)
+// An expression is a JS statement
+( () => { 1 + 1; 2 + 2 } )()      // prints undefined on the console
+// The way we can return a value of an evaluating using the return keyword and any expression on its right side
+( () => { return 1 + 1 } )()      // prints 2 on the console
+// the return statement terminates the function immediately
+// return statement is differeny and behaves differently than expression, it can only be used in blocks
+
+
+// 8
+// And because we can put any expression to the right of the arrow.
+// We can similarly put a function on the right of the arrow as well
+() => () => 0
+// this is a function when applied, returns a function. And when applied again, returns 0
+// similarly, below function when applied twice returns 123
+() => () => 100 + 23
+// lets apply the function:
+( () => () => 0 )()()
